e75178f5042063f2c5fff35ce6e4775d3f35ba97
Jeffery Chen <jefferychen5732@gmail.com>
2022-04-07 16:16:15 -0700

    accepted incomming changes and added spritsheets and tilemaps

--- src/Wolfie2D/Rendering/Animations/ParticleSystem.ts
+++ src/Wolfie2D/Rendering/Animations/ParticleSystem.ts
@@ -6,27 +6,6 @@ import Scene from "../../Scene/Scene";
 import Timer from "../../Timing/Timer";
 import Color from "../../Utils/Color";
 import { EaseFunctionType } from "../../Utils/EaseFunctions";
-<<<<<<< HEAD
-import RandUtils from "../../Utils/RandUtils";
-import ParticleSystemManager from "./ParticleSystemManager";
-
-/*
--Move particle system to HW#4, particle class and particle manager(object pool), source, randomized period of decay,
- semi-randomized approach for spawning, should be general purpose 
- and load some settings from a json (location, states, colors, randomization). 
- Should be effect when balloon is popped 
-*/
-
-export default class ParticleSystem implements Updateable {
-    protected particlePool: Array<Particle>;
-
-    protected lifetime: number;
-
-    protected liveParticles: number;
-
-    protected maxLiveParticles: number;
-
-=======
 import MathUtils from "../../Utils/MathUtils";
 import RandUtils from "../../Utils/RandUtils";
 import ParticleSystemManager from "./ParticleSystemManager";
@@ -38,24 +17,17 @@ export default class ParticleSystem implements Updateable {
     /** Lifetime for each particle */
     protected lifetime: number;
 
->>>>>>> b901612c3cbcefaf7e733e2780eaf245826b3500
     protected sourcePoint: Vec2;
 
     protected particleSize: Vec2;
 
-<<<<<<< HEAD
-=======
     /** Timer for how long a particle system lasts before being turned off */
->>>>>>> b901612c3cbcefaf7e733e2780eaf245826b3500
     protected systemLifetime: Timer;
 
     protected systemRunning: boolean;
 
     protected color: Color = new Color(255, 0, 0);
 
-<<<<<<< HEAD
-    constructor(poolSize: number, sourcePoint: Vec2, lifetime: number, size: number, maxParticles: number) {
-=======
     /** Particles that can be rendered per frame */
     protected particlesPerFrame: number;
 
@@ -75,53 +47,29 @@ export default class ParticleSystem implements Updateable {
      * @param maxParticlesPerFrame Total number of particles that can be created during a given frame.
      */
     constructor(poolSize: number, sourcePoint: Vec2, lifetime: number, size: number, mass: number, maxParticlesPerFrame: number) {
->>>>>>> b901612c3cbcefaf7e733e2780eaf245826b3500
         this.particlePool = new Array(poolSize);
         this.sourcePoint = sourcePoint;
         this.lifetime = lifetime;
         this.particleSize = new Vec2(size, size);
-<<<<<<< HEAD
-        this.maxLiveParticles = maxParticles;
-        this.systemRunning = false;
-=======
         this.systemRunning = false;
         this.particlesPerFrame = maxParticlesPerFrame;
         this.particlesToRender = this.particlesPerFrame;
         this.particleMass = mass;
->>>>>>> b901612c3cbcefaf7e733e2780eaf245826b3500
 
         ParticleSystemManager.getInstance().registerParticleSystem(this);
     }
 
-<<<<<<< HEAD
-    initalizePool(scene: Scene, layer: string, type: ParticleSystemType, mass: number) {
-        for (let i = 0; i < this.particlePool.length; i++) {
-            this.particlePool[i] = <Particle>scene.add.graphic(GraphicType.PARTICLE, layer,
-                { position: this.sourcePoint.clone(), size: this.particleSize.clone(), mass: mass });
-=======
     /** Initialize the pool of all particles, creating the assets in advance */
     initializePool(scene: Scene, layer: string) {
         for (let i = 0; i < this.particlePool.length; i++) {
             this.particlePool[i] = <Particle>scene.add.graphic(GraphicType.PARTICLE, layer,
                 { position: this.sourcePoint.clone(), size: this.particleSize.clone(), mass: this.particleMass });
->>>>>>> b901612c3cbcefaf7e733e2780eaf245826b3500
             this.particlePool[i].addPhysics();
             this.particlePool[i].isCollidable = false;
             this.particlePool[i].visible = false;
         }
     }
 
-<<<<<<< HEAD
-    startSystem(time: number, startPoint?: Vec2) {
-        this.systemLifetime = new Timer(time);
-        this.systemLifetime.start();
-        this.systemRunning = true;
-        this.sourcePoint = startPoint;
-    }
-
-    stopSystem() {
-        console.log(this);
-=======
     /**
      * Start up the particle system to run for a set amount of time
      * @param time Time for the particle systme to run
@@ -149,7 +97,6 @@ export default class ParticleSystem implements Updateable {
     }
 
     stopSystem() {
->>>>>>> b901612c3cbcefaf7e733e2780eaf245826b3500
         this.systemRunning = false;
         for (let particle of this.particlePool) {
             if (particle.inUse) {
@@ -162,12 +109,6 @@ export default class ParticleSystem implements Updateable {
         this.color = color;
     }
 
-<<<<<<< HEAD
-    update(deltaT: number) {
-        if (!this.systemRunning) {
-            return;
-        }
-=======
     /**
      * Default implementation of setParticleAnimation, no tween animations occur, but each particle is given a random
      * velocity. It's encouraged for you to override this function and implement your own tween animations.
@@ -189,19 +130,14 @@ export default class ParticleSystem implements Updateable {
             return;
         }
         // Stop the system if our timer is up
->>>>>>> b901612c3cbcefaf7e733e2780eaf245826b3500
         if (this.systemLifetime.isStopped()) {
             this.stopSystem();
         }
         else {
-<<<<<<< HEAD
-            for (let particle of this.particlePool) {
-=======
             for (let i = 0; i < this.particlesToRender; i++) {
                 let particle = this.particlePool[i];
 
                 // If a particle is in use, decrease it's age and update it's velocity, if it has one
->>>>>>> b901612c3cbcefaf7e733e2780eaf245826b3500
                 if (particle.inUse) {
                     particle.decrementAge(deltaT * 1000);
 
@@ -209,67 +145,6 @@ export default class ParticleSystem implements Updateable {
                         particle.setParticleInactive();
                     }
 
-<<<<<<< HEAD
-                    //particle.vel.y += 200*deltaT;
-                    particle.move(particle.vel.scaled(deltaT));
-                }
-                else {
-                    particle.setParticleActive(this.lifetime, this.sourcePoint.clone());
-
-                    particle.color = this.color;
-                    particle.alpha = 1;
-                    //particle.size.set(1)
-                    particle.vel = RandUtils.randVec(-50, 50, -100, 100);
-
-                    particle.tweens.add("active", {
-                        startDelay: 0,
-                        duration: 2000,
-                        effects: [
-                            {
-                                property: "alpha",
-                                resetOnComplete: true,
-                                start: 1,
-                                end: 0,
-                                ease: EaseFunctionType.IN_OUT_SINE
-                            },
-                            /*{
-                                property: "colorR",
-                                resetOnComplete: true,
-                                start: particle.color.r,
-                                end: 255,
-                                ease: EaseFunctionType.IN_OUT_SINE
-                            },
-                            {
-                                property: "colorG",
-                                resetOnComplete: true,
-                                start: particle.color.g,
-                                end: 255,
-                                ease: EaseFunctionType.IN_OUT_SINE
-                            },
-                            {
-                                property: "colorB",
-                                resetOnComplete: true,
-                                start: particle.color.b,
-                                end: 255,
-                                ease: EaseFunctionType.IN_OUT_SINE
-                            },*/
-                            {
-                                property: "velY",
-                                resetOnComplete: true,
-                                start: particle.vel.y,
-                                end: particle.vel.y + ((this.lifetime * particle.mass)/2),
-                                ease: EaseFunctionType.IN_OUT_SINE
-                            }
-                        ]
-                    });
-
-                    particle.tweens.play("active");
-
-                    //particle.vel = RandUtils.randVec(-150, 150, -100, 100);
-                    //console.log(particle.vel.toString());
-                }
-            }
-=======
                     particle.move(particle.vel.scaled(deltaT));
                 }
                 else {
@@ -290,16 +165,7 @@ export default class ParticleSystem implements Updateable {
             // Update the amount of particles that can be rendered based on the particles per frame, clamping if we go over the total number
             // of particles in our pool
             this.particlesToRender = MathUtils.clamp(this.particlesToRender+this.particlesPerFrame, 0, this.particlePool.length);
->>>>>>> b901612c3cbcefaf7e733e2780eaf245826b3500
         }
     }
 
 }
-<<<<<<< HEAD
-
-export enum ParticleSystemType {
-    emitter = "emitter",
-    burst = "burst"
-}
-=======
->>>>>>> b901612c3cbcefaf7e733e2780eaf245826b3500
