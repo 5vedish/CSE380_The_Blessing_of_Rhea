116ee3a7f2678b4e1be6056ee6173247c7c9d9d1
ntan03 <nelson.tan@stonybrook.edu>
2022-04-07 20:15:57 -0400

    Project Enumerations

---
+++ src/ProjectEnums.ts
@@ -0,0 +1,11 @@
+export enum Project_Events {
+    MOVE = "Move",
+    ATTACK = "Attack",
+    HIT = "Hit",
+    DAMAGE = "Damage",
+    COLLISION = "Collision",
+    KILLED = "Killed",
+    PLAYER_UTILITY = "PlayerUtility",
+    LEVEL_START = "LevelStart",
+    LEVEL_END = "LevelEnd"
+}
