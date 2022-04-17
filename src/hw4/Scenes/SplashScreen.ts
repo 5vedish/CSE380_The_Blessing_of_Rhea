2b1b5e7c0983b0a287601eb0594aa41554359215
Zhixing Zhao <zhixing.zhao@stonybrook.edu>
2022-04-17 18:06:05 -0400

    Click Label Flashing

--- src/hw4/Scenes/SplashScreen.ts
+++ src/hw4/Scenes/SplashScreen.ts
@@ -6,19 +6,26 @@ import Color from "../../Wolfie2D/Utils/Color";
 import Label from "../../Wolfie2D/Nodes/UIElements/Label";
 import MainMenu from "./MainMenu";
 import Sprite from "../../Wolfie2D/Nodes/Sprites/Sprite";
+import Timer from "../../Wolfie2D/Timing/Timer";
 
 export default class SplashScreen extends Scene {
+
   private splashScreen: Layer;
   private bg: Sprite;
+  private startLabel: Sprite;
+  private flash: Timer;
 
   loadScene() {
     this.load.image("splash_screen", "project_assets/screens/Splash.png");
+    this.load.image("start_label", "project_assets/sprites/start_label.png");
   }
 
   startScene() {
     this.splashScreen = this.addUILayer("splashScreen");
     this.bg = this.add.sprite("splash_screen", "splashScreen");
     this.bg.position.copy(this.viewport.getCenter());
+    this.flash = new Timer(1000, null, true);
+    this.flash.start();
 
     // Add click-to-start button
     const clickStart = this.add.uiElement(
@@ -32,13 +39,16 @@ export default class SplashScreen extends Scene {
     clickStart.backgroundColor = Color.TRANSPARENT;
     clickStart.onClickEventId = "start";
 
-    const clickLabel = <Label>this.add.uiElement(UIElementType.LABEL, "splashScreen", {position: new Vec2(800, 675), text: "Click to Start"})
-    clickLabel.textColor = Color.BROWN;
-
+    this.startLabel = this.add.sprite("start_label", "splashScreen");
+    this.startLabel.position = new Vec2(800, 675);
+    
     this.receiver.subscribe("start");
   }
 
   updateScene() {
+
+    this.startLabel.alpha = 2* Math.abs(this.flash.getTimeLeft()/1000 - .5);
+
     while (this.receiver.hasNextEvent()) {
       let event = this.receiver.getNextEvent();
       console.log(event);
