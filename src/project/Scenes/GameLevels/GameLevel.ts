6f436a2e85b40aefc21584f7931966bdb1bfc16e
ntan03 <nelson.tan@stonybrook.edu>
2022-05-09 02:04:39 -0400

    Updated inventory rows

--- src/project/Scenes/GameLevels/GameLevel.ts
+++ src/project/Scenes/GameLevels/GameLevel.ts
@@ -284,7 +284,7 @@ export default class GameLevel extends Scene{
 
         // Inventory Box Label
         let inventoryBorder = this.add.graphic(GraphicType.RECT, "gui", {position: new Vec2(this.viewport.getHalfSize().x + 216.5, this.viewport.getHalfSize().y - 179), 
-            size: new Vec2(320, 120)});
+            size: new Vec2(320, 144)});
         inventoryBorder.color = Color.BLACK;
         inventoryBorder.alpha = .5;
 
